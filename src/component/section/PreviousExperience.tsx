220a15a4e29b45a717cc1acd6637b556db682003
Alabi Damilola Daniel <eminencemailx@gmail.com>
2022-10-19 19:07:27 -0700

    experience updated

--- src/component/section/PreviousExperience.tsx
+++ src/component/section/PreviousExperience.tsx
@@ -9,44 +9,59 @@ const PreviousExperience =() => {
     useEffect(() => {
         AOS.init({duration : 800});
       }, [])
+  const experienceInfo =[
+    {
+        image :"/hostinza.png",
+        title :"Nodejs Developer ",
+        date :"September 2022",
+        jobDescription :"I setup   stripe payment workflow on Hostinza with Nodejs  and connected with NextJS  frontend, built some API endpoints  and cofigured the platform for matomo analytics"
+    } , 
+    {
+        image :"/playjor2.jpg",
+        title :"NextJs frontend Developer",
+        date :"July 2022",
+        jobDescription :" I worked together with a team of backend dev and front end dev to setup a marketplace on Playjor platform"
+    } , 
+    {
+        image :"/fiverr.png",
+        title :"Freelance Web Designer",
+        date :"May 2021",
+        jobDescription :"As a freelance  Web designer , I designed and redesigned websites for clients on CMS platforms like Wordpress, squarespace, Shopify e.t.c  Check out projects on the CMS tab of my project page"
+    } 
+  ]
     return (
         <div className="pExperience">
             <div className="experience">
                 <h1>Work Experience</h1>
                 <hr/>
-                <div data-aos="fade-up" className="company">
-                    <div className="companyLogoCon">
-                        <img src="/playjor2.jpg"  alt=""/>
-                    </div>
-                    <div className="workExperience">
-                        <h3>NextJs frontend Developer</h3>
-                        <h5>July 2022</h5>
-                         <p>I was a frontend webdev @ plajor where I worked together with a team of backend dev and front end dev to build and setUp the PlayJor platform proficiently</p>
-                        <hr/>
-                    </div>
-                </div>
-                <div data-aos="fade-up" className="company">
-                    <div className="companyLogoCon">
-                        <img src="/fiverr.png"  alt=""/>
-                    </div>
-                    <div className="workExperience">
-                        <h3>Freelance Frontend Developer</h3>
-                        <h5>May 2021</h5>
-                         <p>Having been a frontend dev on fiverr, I have been able to implement my proficiency in technologies like HTML,CSS, JAVASCRIPT and REACT.      Check out my fiverr profile <a target='_blank' href='https://www.fiverr.com/emilance'>HERE</a>  </p>
-                        <hr/>
-                    </div>
-                </div>
+                {experienceInfo.map((data, index)=>{
+                    return(
+                        <div data-aos="fade-up" className="company">
+                        <div className="companyLogoCon">
+                            <img src={data.image}  alt=""/>
+                        </div>
+                        <div className="workExperience">
+                            <h3>{data.title}</h3>
+                            <h5>{data.date}</h5>
+                             <p>{data.jobDescription}</p>
+                            <hr/>
+                        </div>
+                        </div>
+                    )
+                })}
+              
                 <div data-aos="fade-up" className="company">
                     <div className="companyLogoCon">
                         <img src="/fiverr1.png"  alt=""/>
                     </div>
                     <div className="workExperience">
-                        <h3>Freelance Web Designer</h3>
+                        <h3>Freelance Frontend Developer</h3>
                         <h5>May 2021</h5>
-                         <p>I was a freelance  Web designer on Fiverr micro jobs platform where I designed and redesigned websites for clients on CMS platforms like Wordpress, squarespace, Shopify e.t.c  Check out my fiverr profile <a target="_blank" href='https://www.fiverr.com/emilance'>HERE</a>   </p>
+                         <p>As a freelance frontend developer on fiverr, I have been able to implement my proficiency in technologies like HTML,CSS, JAVASCRIPT and REACT.      Check out my fiverr profile <a target='_blank' href='https://www.fiverr.com/emilance'>HERE</a>  </p>
                         <hr/>
                     </div>
                 </div>
+              
             </div>
 
             
